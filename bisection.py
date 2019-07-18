ffd9842b18d3795dccac8577551cebe3eea7a89b
gabrielrmachado <gabriel.rmachado10@gmail.com>
2019-07-18 12:31:26 -0300

    minor changes

--- bisection.py
+++ bisection.py
@@ -1,25 +1,29 @@
 import math
+import numpy as np
 
 def function(x):
-    return math.pow(math.e, -x) * math.cos(x)
+    # return math.pow(math.e, -x) * math.cos(x)
+    return ((math.pow(x,3)) - (7*math.pow(x,2)) + (14*x) - 6)
 
 def bisection(a, b, max_iter, TOL, counter):
     if counter < max_iter:
         counter = counter + 1
-        if abs(function(a)) <= TOL:
+        ans1 = function(a)
+        ans2 = function(b)
+        if abs(ans1) <= TOL:
             print("Root found on iteration {0}.\nf({1}) = {2:.10f}".format(counter, a, function(a)))
-        elif abs(function(b)) <= TOL:
+        elif abs(ans2) <= TOL:
                 print("Root found on iteration {0}.\nf({1}) = {2:.10f}".format(counter, b, function(b)))
-        else:
-            p = a + (b-a)/2
+        elif ans1*ans2 < 0:
+            p = (a+b)/2
             ans = function(p)
             print("Iter. {0}: f({1}) = {2}".format(counter, p, ans))
             
-            if (ans > 0):
+            if np.sign(ans) == np.sign(ans2):
                 bisection(a, p, max_iter, TOL, counter)
             else:
                 bisection(p, b, max_iter, TOL, counter)
     else:
         print("Number of max iterations reached. No root was found.")
         
-bisection(math.pi, 0, 150, 0.0000000001, 0)
+bisection(3.2, 4, 150, 0.001, 0)
--- newton_raphson.py
+++ newton_raphson.py
@@ -2,15 +2,16 @@ import math
 
 def function(x):
     # return math.pow(math.e, -x) * math.cos(x)
-    return math.pow(math.e, x)
+#     return math.pow(math.e, x)
     # return 4 * math.pow(x, 3) + 3 *  math.pow(x, 2) - 7*x
+    return ((math.pow(x,3)) - (7*math.pow(x,2)) + (14*x) - 6)
 
 def ddx_function(x):
     # return (- math.pow(math.e, -x) * (math.sin(x) + math.cos(x)))
-    return (math.pow(math.e, x))
-    # return 12 * math.pow(x, 2) + 6*x - 7
+#     return (math.pow(math.e, x))
+    return 3 * math.pow(x, 2) - 14*x + 14
     
-def newton_raphson(x, i=0, max_iter = 50, TOL = 0.000000001):
+def newton_raphson(x, i=0, max_iter = 50, TOL = 0.001):
     ans = function(x)
     if (i < max_iter):
         if (abs(ans) <= TOL):
@@ -23,4 +24,4 @@ def newton_raphson(x, i=0, max_iter = 50, TOL = 0.000000001):
     else:
         print("Number of max iterations reached. No root was found.")
 
-newton_raphson(-10)
+newton_raphson(2)
