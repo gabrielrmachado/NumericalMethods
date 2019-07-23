e87fc8f0309a7337b65ec1a40042786472ab3a13
gabrielrmachado <gabriel.rmachado10@gmail.com>
2019-07-23 11:37:53 -0300

    Plotting fixes

--- Interpolation/lagrange.py
+++ Interpolation/lagrange.py
@@ -3,18 +3,19 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 
-x = np.array([1.0, 5.7, 15])
-y = np.vectorize((lambda a: a * math.log(a)))
+x = np.array([0, math.pi/4, math.pi/2, math.pi])
+y = np.vectorize((lambda a: math.sin(a)))
 
-testPoint = 10
+testPoint = 3.2
 f = lagrange(x, y(x))
 absError = abs(y(testPoint)-f(testPoint))
 
 print("{0}\nP({1}) = {2}\nAbsolute error: {3:.10f}".format(f, testPoint, f(testPoint), absError))
 
-plt.figure()
-plt.plot([1,3,6,9,12,18], y([1,3,6,9,12,18]), label='True function F(x)')
+plt.figure("Lagrange")
+plt.plot(np.arange(0.5,10,0.5), y(np.arange(0.5,10,0.5)), label='True function F(x)')
 plt.plot(x, f(x), '--k', label='Interpolation polynomial P(x)')
 plt.plot(x, y(x), 'ok', label='Given points')
+plt.plot(testPoint, f(testPoint), 'or', label='Test point')
 plt.legend()
 plt.show()
